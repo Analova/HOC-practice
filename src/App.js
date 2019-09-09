97b7ec9faf250a07d3ec46005db5a615eac5c8f0
Ana Nikolova <ani.nik19@gmail.com>
2019-09-09 13:21:15 +0200

    created UserList component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.scss";
 
 import UserProfile from "./components/user-profile/user-profile.component";
-
+import UserList from "./components/user-list/user-list.component";
 function App() {
   return (
     <div className="App">
+      <UserList />
       <UserProfile name="Jane Morris" email="morrisJane@gmail.com" />
     </div>
   );
