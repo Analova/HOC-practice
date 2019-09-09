255aa12ac4f0e37e6fbc2db0992f85655f59649e
Ana Nikolova <ani.nik19@gmail.com>
2019-09-09 13:47:52 +0200

    passed HOC withData to UserList and UserProfile Component

--- src/components/user-list/user-list.component.jsx
+++ src/components/user-list/user-list.component.jsx
@@ -1,31 +1,19 @@
 import React from "react";
 
-class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: []
-    };
-  }
+import withData from "../../with-data";
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => this.setState({ users: data.slice(0, 3) }));
-  }
-  render() {
-    return (
-      <div className="container user-list">
-        <h1> User List</h1>
-        {this.state.users.map(user => (
-          <div className="post" key={user.id}>
-            <h1>{user.name}</h1>
-            <h2>{user.email}</h2>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const UserList = ({ data }) => {
+  return (
+    <div className="container user-list">
+      <h1> User List</h1>
+      {data.map(user => (
+        <div className="post" key={user.id}>
+          <h1>{user.name}</h1>
+          <h2>{user.email}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default UserList;
+export default withData(UserList, "https://jsonplaceholder.typicode.com/users");
--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -1,33 +1,24 @@
 import React from "react";
 
-class UserProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
+import withData from "../../with-data";
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(data => this.setState({ posts: data.slice(0, 3) }));
-  }
-  render() {
-    return (
-      <div className="container">
-        <h1>{this.props.name}</h1>
-        <h2>{this.props.email}</h2>
-        Posts:
-        {this.state.posts.map(post => (
-          <div className="post">
-            <h1>{post.title}</h1>
-            <h2>{post.body}</h2>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const UserProfile = ({ data, name, email }) => {
+  return (
+    <div className="container">
+      <h1>{name}</h1>
+      <h2>{email}</h2>
+      Posts:
+      {data.map(post => (
+        <div className="post">
+          <h1>{post.title}</h1>
+          <h2>{post.body}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default UserProfile;
+export default withData(
+  UserProfile,
+  "https://jsonplaceholder.typicode.com/posts"
+);
